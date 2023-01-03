49e22e9cd9cbf093225174b53639137d2237901e
rafa238 <rafalaureano246@gmail.com>
2023-01-03 17:34:25 -0600

    Condiciale when terminada :D

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -36,11 +36,24 @@ fun condicionales(){
     println(mensaje)
 }
 
-fun whenCodicional(){
-    val nombreColor = "Rojo"
-
+fun whenCodicional(nombreColor: String, codigo: Int){
+    when (nombreColor) {
+        "Amarillo" -> println("Este color simboliza la felicidad")
+        //con la soma incluimos nuevos casos en donde se va a entrar
+        "Rojo", "Carmesi" -> println("Este color es el color de la sangre")
+        else -> println("No me diste ningun color")
+    }
+    //rangos, podemos checar si nuestra variable se encuentra en un rango
+    // notese que al igual que if, podemos hacer una asignacion atravez de la sentencia
+    val mensaje =
+    when(codigo){
+        in 100..299 -> "Todo ha salido bien"
+        in 300..599 -> "Ha habido algun error"
+        else -> "Error desconocido"
+    }
+    println(mensaje)
 }
 
 fun main(args: Array<String>) {
-    condicionales()
+    whenCodicional("Carmesi", 150)
 }
